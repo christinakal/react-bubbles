a27511f9969a45a3aa16b179a99fa3401f499f0b
christina kaliora <develop@pirron.one>
2020-01-31 17:29:01 +0100

    login page in progress - light styling added

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [user, setUser] = useState({
+    username: "",
+    password: ""
+  });
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form className="login-form">
+        <h1 className="welcome">Welcome to the Bubble App!</h1>
+        <label>Username</label>
+        <input type="text" name="username" placeholder="username"></input>
+        <label>Password</label>
+        <input type="password" name="username" placeholder="password"></input>
+        <button>Login</button>
+      </form>
     </>
   );
 };
